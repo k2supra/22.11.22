6695b06cffa4090da0b485b14e32b84f777d6029
k2supra <andrijkorolevic@gmail.com>
2022-11-22 20:14:24 +0200

    Created new main

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-message = tuple("olo, lrgg")
-print(message[7])
+import random
+try:
+    size = int(input("Size ->"))
+    begin = int(input("Begin ->"))
+    finish = int(input("Finish ->"))
+    list = []
+
+    for i in range(size):
+        list.append(random.randint(begin, finish))
+
+    list.sort()
+    for item in list:
+        print(item, end=" ")
+    print()
+
+    dict = (list[:1], list[-1])
+    print(dict)
+
+    #lis = tuple(list)
+    #print(lis[:1], lis[size - 1])
+except Exception as ex:
+    print(f"Error: {ex}")
